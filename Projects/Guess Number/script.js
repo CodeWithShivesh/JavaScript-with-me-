333f80c5cd126fca314f2fec14a23529db4d5bf2
Shivesh Mishra <shiveshthestudytop@gmail.com>
2024-09-10 18:31:32 +0000

    game completed

--- Projects/Guess Number/script.js
+++ Projects/Guess Number/script.js
@@ -6,10 +6,14 @@ const input = document.querySelector("#guessField");
 const previousValue = document.querySelector('.guesses');
 const submit = document.getElementById('subt');
 const message = document.querySelector('.lowOrHi');
+const count = document.querySelector('.lastResult');
+const warning = document.querySelector('warning');
 
 let game = true;
 const msg = document.createElement('h2');
 let preVal = [];
+let num = 10;
+
 
 if (game) {
     submit.addEventListener('click', function (e) {
@@ -21,6 +25,8 @@ if (game) {
         checkGuess(guess, randmNumber)
     })
 }
+
+
 function checkGuess(Guess, randmNumber) {
     if (isNaN(Guess)) {
         alert("Enter valid number!!");
@@ -48,4 +54,17 @@ function validateGuess(Guess, randmNumber) {
 function displayMessage(msg, Guess) {
     message.innerHTML = `<h2>${msg}</h2>`
     previousValue.innerHTML += `${Guess}  `;
+    num -= 1;
+    count.innerHTML = num;
+    if (num < 1) endGame();
+}
+
+function endGame() {
+    if (num < 1) {
+        game = false;
+        input.setAttribute('disabled', '');
+        message.innerHTML = `<h2>You lost all the chance !!</h2>`
+        warning.style.color = "Red";
+
+    }
 }
